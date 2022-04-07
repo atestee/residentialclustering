31c66bcf5e7560e727ab9e2ac7e8729c398460d2
atestee <ester@atlasova.cz>
2022-04-07 11:15:29 +0200

    feat(App.js): HighLevelNavigation and DetailedNavigation route removed and instead VisualizationNavigator added, which serves as a wrapper component for the former components

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import {CityPicker} from "./components/CityPicker/CityPicker";
 import {RoutePicker} from "./components/RoutePicker/RoutePicker";
 import {CenterPicker} from "./components/CenterPicker/CenterPicker";
 import {ParametersFormPage} from "./components/ParametersFormPage/ParametersFormPage";
-import {HighLevelViz} from "./components/HighLevelViz/HighLevelViz";
-import {DetailedViz} from "./components/DetailedViz/DetailedViz";
+import {HighLevelViz} from "./components/Visualization/HighLevelViz/HighLevelViz";
+import {DetailedViz} from "./components/Visualization/DetailedViz/DetailedViz";
+import {VisualizationNavigator} from "./components/Visualization/VisualizationNavigator";
 
 export class App extends Component {
     myStorage = window.localStorage;
@@ -20,13 +21,14 @@ export class App extends Component {
             <Router>
                 <div>
                     <Routes>
-                        <Route path="/" element={<HighLevelViz storage={this.myStorage} />}/>
-                        <Route path="/detailed-visualization" element={<DetailedViz storage={this.myStorage} />}/>
+                        {/*<Route path="/" element={<HighLevelViz storage={this.myStorage} />}/>*/}
+                        {/*<Route path="/detailed-visualization" element={<DetailedViz storage={this.myStorage} />}/>*/}
                         <Route path="/new-job/1" element={<CityPicker storage={this.myStorage} />} />
                         <Route path="/new-job/2" element={<RoutePicker storage={this.myStorage} />} />
                         <Route path="/new-job/3" element={<CenterPicker storage={this.myStorage} />} />
                         <Route path="/new-job/4" element={<ParametersFormPage storage={this.myStorage} />} />
                         <Route path="/job-management" element={<JobManagerPicker storage={this.myStorage} />} />
+                        <Route path="/jobs/:jobId" element={<VisualizationNavigator storage={this.myStorage} />} />
                     </Routes>
                 </div>
             </Router>
