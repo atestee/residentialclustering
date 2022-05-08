6363e97cfe6aaa0a8b04cdec361742eab4f61f20
atestee <ester@atlasova.cz>
2022-05-08 18:36:22 +0200

    refactor: VisualizationNavigator

--- src/components/Visualization/VisualizationNavigator.js
+++ src/components/Visualization/VisualizationNavigator.js
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 import {HighLevelViz} from "./HighLevelViz/HighLevelViz";
 import {DetailedViz} from "./DetailedViz/DetailedViz";
-import testResponse from '../../data/test_response.json'
 import {useEffect, useState} from "react";
+import {CircularProgress} from "@mui/material";
 
 const HIGH_LEVEL = 0;
 const DETAILED = 1;
@@ -10,26 +10,55 @@ const DETAILED = 1;
 export function VisualizationNavigator ({storage}) {
     let { jobId } = useParams(); // for the GET request - to receive the analysis data
     let [shownComponent, setShownComponent] = useState(HIGH_LEVEL);
-    let [shownClusterId, setShownClusterId] = useState(null);
+    let [shownCluster, setShownCluster] = useState(null);
+    let [shownClusterName, setShownClusterName] = useState(null);
+    let [shownClusterIdx, setShownClusterIdx] = useState(null);
+    let [jobData, setJobData] = useState(null);
 
     useEffect(() => {
-    })
+        fetch("http://localhost:5000/api/finished-job/" + jobId)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+
+            })
+            .then((data) => {
+                setJobData(data)
+            })
+            .catch((error) => {
+                console.error("Error fetching data: " + error);
+            })
+    }, [jobId])
 
     let showHighLevelViz = () => {
         setShownComponent(HIGH_LEVEL);
     };
 
-    let showDetailedViz = (clusterId) => {
+    let showDetailedViz = (clusterIdx, clusterName) => {
+        setShownClusterIdx(clusterIdx);
+        setShownClusterName(clusterName);
+        setShownCluster(jobData["clusters"][clusterIdx]);
         setShownComponent(DETAILED);
-        setShownClusterId(clusterId);
     };
 
-    switch (shownComponent) {
-        case HIGH_LEVEL:
-            return (<HighLevelViz showDetialedViz={showDetailedViz.bind(this)} analysisData={testResponse} storage={storage}/>);
-        case DETAILED:
-            return (<DetailedViz showHighLevelViz={showHighLevelViz.bind(this)} analysisData={testResponse[{shownClusterId}]} clusterId={shownClusterId} storage={storage}/>);
-        default:
-            return (<HighLevelViz showDetialedViz={showDetailedViz.bind(this)} analysisData={testResponse} storage={storage}/>);
+
+    if (jobData) {
+        switch (shownComponent) {
+            case HIGH_LEVEL:
+                return (<HighLevelViz showDetailedViz={showDetailedViz.bind(this)} analysisData={jobData["clusters"]} parameters={jobData["parameters"]} storage={storage}/>);
+            case DETAILED:
+                return (<DetailedViz showHighLevelViz={showHighLevelViz.bind(this)} clusterData={shownCluster} parameters={jobData["parameters"]} clusterId={shownClusterIdx} clusterName={shownClusterName} storage={storage}/>);
+            default:
+                return (<HighLevelViz showDetailedViz={showDetailedViz.bind(this)} analysisData={jobData} parameters={jobData["parameters"]} storage={storage}/>);
+        }
+    } else {
+        return (
+            <div style={{ display: "flex", height: "100vh"}}>
+                <CircularProgress size={100} style={{ margin: "auto" }}/>
+            </div>
+        )
     }
+
 }
